569c1c93170093e20f9b497120d0a065d882f0d2
Piyush Tiwari <piyush@Piyushs-MacBook-Pro.local>
2023-06-27 19:50:07 +0530

    conversions

---
+++ conversion/camel_to_snake_case.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	jsonFile, err := os.Open("request.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer jsonFile.Close()
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var data map[string]interface{}
+	err = json.Unmarshal(byteValue, &data)
+	if err != nil {
+		fmt.Printf("Failed to unmarshal JSON: %v", err)
+		return
+	}
+
+	convertedData := convertFieldNames(data)
+
+	// Marshal the converted data back to JSON
+	convertedJSON, err := json.Marshal(convertedData)
+	if err != nil {
+		fmt.Printf("Failed to marshal JSON: %v", err)
+		return
+	}
+
+	err = ioutil.WriteFile("response.json", convertedJSON, 0600)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func convertFieldNames(data interface{}) interface{} {
+	value := reflect.ValueOf(data)
+	switch value.Kind() {
+	case reflect.Map:
+		newMap := make(map[string]interface{})
+		iter := value.MapRange()
+		for iter.Next() {
+			key := iter.Key().String()
+			if strings.Contains(key, "CustomInfo") {
+				continue
+			}
+
+			if strings.Contains(key, "centAmount") {
+				ca := iter.Value().Interface().(string)
+				centAmt, err := strconv.Atoi(ca)
+				if err != nil {
+					return ca
+				}
+				fieldName := camelToSnakeCase(key)
+				var centAmtInt interface{} = centAmt
+				newMap[fieldName] = convertFieldNames(centAmtInt)
+				continue
+			}
+
+			fieldName := camelToSnakeCase(key)
+			newMap[fieldName] = convertFieldNames(iter.Value().Interface())
+		}
+		return newMap
+	case reflect.Slice:
+		newSlice := make([]interface{}, value.Len())
+		for i := 0; i < value.Len(); i++ {
+			newSlice[i] = convertFieldNames(value.Index(i).Interface())
+		}
+		return newSlice
+	case reflect.Struct:
+		newStruct := make(map[string]interface{})
+		for i := 0; i < value.NumField(); i++ {
+			field := value.Field(i)
+			fieldName := value.Type().Field(i).Name
+			if strings.Contains(fieldName, "CustomInfo") {
+				continue
+			}
+			fieldValue := convertFieldNames(field.Interface())
+			newStruct[camelToSnakeCase(fieldName)] = fieldValue
+		}
+		return newStruct
+	default:
+		return data
+	}
+}
+
+// Function to convert camel case to snake case
+func camelToSnakeCase(s string) string {
+	var result strings.Builder
+
+	for i, ch := range s {
+		if i > 0 && (ch >= 'A' && ch <= 'Z') {
+			result.WriteByte('_')
+		}
+		result.WriteRune(ch)
+	}
+
+	return strings.ToLower(result.String())
+}
---
+++ conversion/string_tree_conversion.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type node struct {
+	Name     string  `json:"name"`
+	Children []*node `json:"children,omitempty"`
+}
+
+func parse(v string) (*node, error) {
+	root := &node{}
+
+	parseChildren(v, root)
+
+	return root, nil
+}
+
+func parseChildren(v string, n *node) {
+	if v == "" {
+		return
+	}
+
+	name := ""
+	i := 0
+	// getting name(parent) string from the string
+	if v[i] != '[' {
+		for ; i < len(v); i++ {
+			if v[i] == '[' {
+				break
+			}
+
+			name += string(v[i])
+		}
+	}
+
+	n.Name = name
+
+	arr := []string{}
+	k := i + 1
+	// bracket to count open & close brackets
+	bracket := 0
+	childString := ""
+
+	// spliting the string into array if we found ','
+	for i = i + 1; i < len(v)-1; i++ {
+		if v[i] == '[' {
+			bracket++
+		}
+		if v[i] == ']' {
+			bracket--
+		}
+		if v[i] == ',' && bracket == 0 {
+			childString = v[k:i]
+			arr = append(arr, childString)
+			k = i + 1
+		}
+	}
+
+	// this will become true when we have no brackets & comma in string(completing the recursion)
+	if k == i {
+		return
+	}
+
+	childString = v[k:i]
+	arr = append(arr, childString)
+
+	// calling parseChildren for each string in array
+	for i := 0; i < len(arr); i++ {
+		n.Children = append(n.Children, &node{})
+
+		parseChildren(arr[i], n.Children[i])
+	}
+}
+
+var examples = []string{
+	"[a,b,c]",
+	"[a[aa[aaa],ab,ac],b,c[ca,cb,cc[cca]]]",
+}
+
+func main() {
+	for i, example := range examples {
+		result, err := parse(example)
+		if err != nil {
+			panic(err)
+		}
+		j, err := json.MarshalIndent(result, " ", " ")
+		if err != nil {
+			panic(err)
+		}
+
+		log.Printf("Example %d: %s - %s", i, example, string(j))
+	}
+}
