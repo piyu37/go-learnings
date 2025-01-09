256def055ec8d11d1ca0df4d1ea0ff9867222b97
Piyush Tiwari <ptiwari248@gmail.com>
2025-01-09 11:32:28 +0530

    Learning recursion in depth.

--- recursion/powerset_recur.go
+++ recursion/powerset_recur.go
@@ -25,9 +25,31 @@ func powersetHelper(array []int, index int) [][]int {
 	return subsets
 }
 
+// This approach we follow in many recursions; like pick and not pick
+// example of real world is like when we try clothes, to wear new onle we need to remove old
+// following the same in below approach too
+func PowersetRecur2(array []int) [][]int {
+	subsets := [][]int{}
+	powersetHelper2(array, 0, []int{}, &subsets)
+	return subsets
+}
+
+func powersetHelper2(array []int, index int, tempArr []int, subsets *[][]int) {
+	if index >= len(array) {
+		*subsets = append(*subsets, tempArr)
+		return
+	}
+
+	tempArr = append(tempArr, array[index]) // pick
+	powersetHelper2(array, index+1, tempArr, subsets)
+	tempArr = tempArr[:len(tempArr)-1] // not pick
+	powersetHelper2(array, index+1, tempArr, subsets)
+}
+
 // https://github.com/lee-hen/Algoexpert/tree/master/medium/25_powerset
 func powersetRecurMain() {
 	arr := []int{1, 2, 3, 4, 5}
 
 	fmt.Println(PowersetRecur(arr))
+	fmt.Println(PowersetRecur2(arr))
 }
